5cdabc5f082acf97d1308f384811b4860b99b16f
Tommy A Tran <tommy.anh.tran@ibm.com>
2021-05-28 14:55:53 -0500

    import and export components

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import FirstComponent, { SecondComponent } from "./components/learning-examples/FirstComponent.js";
 
 function App() {
   return (
     <div className="App">
-      Tommy's Todo App
+      <h1>Tommy's Todo App</h1>
+      <FirstComponent></FirstComponent>
+      <SecondComponent></SecondComponent>
+      <ThirdComponent></ThirdComponent>
     </div>
   );
+}
+
+
+
+function ThirdComponent() {
+  return <div className="thirdComponent">Third Component!</div>;
 }
 
 export default App;
---
+++ todo-app/src/components/learning-examples/FirstComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function FirstComponent() {
+    return (
+        <div className="firstComponent">
+            First Component!
+        </div>
+    )
+}
+
+export function SecondComponent() {
+    return <div className="secondComponent">Second Component!</div>;
+  }
