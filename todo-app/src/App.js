5d738a34697154180ce5fa3c6c5623c756488a3a
Tommy A Tran <tommy.anh.tran@ibm.com>
2021-05-28 12:22:33 -0500

    creating components

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,12 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
+// This is the Root of hte APP!
 function App() {
   return (
     <div className="App">
-      
+      My Hello World
+      <FirstComponent></FirstComponent>
+      <SecondComponent></SecondComponent>
     </div>
   );
 }
+
+function FirstComponent() {
+  return (
+    <div className="firstComponent">
+    Testing New Component
+    </div >
+  )
+}
+
+function SecondComponent() {
+  return (
+    <div className="secondComponent">
+    Component number 3, yes this is my third component
+    </div >
+  )
+}
+
+
 
 export default App;
