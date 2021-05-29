2ec6d1ce817fbbb3f83742a916b6678d601094e2
Tommy <zombietommie@gmail.com>
2021-05-29 00:11:04 -0500

    Update App.js

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -9,6 +9,7 @@ function App() {
 		<div className='App'>
 			<h1>Tommy's Todo App</h1>
 			<FirstComponent></FirstComponent>
+			<SecondComponent> </SecondComponent>
 			<ThirdComponent></ThirdComponent>
 		</div>
 	);
