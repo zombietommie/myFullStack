b6034453c87b847cc6499a056a17d8ebfde7726e
Tommy <zombietommie@gmail.com>
2021-05-28 22:04:01 -0500

    Update App.js

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,18 +1,17 @@
-import React from "react";
-import "./App.css";
-import FirstComponent from "./components/learning-examples/FirstComponent";
-import SecondComponent from "./components/learning-examples/SecondComponent";
-import ThirdComponent from "./components/learning-examples/ThirdComponent";
+import React from 'react';
+import './App.css';
+import FirstComponent from './components/learning-examples/FirstComponent';
+import SecondComponent from './components/learning-examples/SecondComponent';
+import ThirdComponent from './components/learning-examples/ThirdComponent';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Tommy's Todo App</h1>
-      <FirstComponent></FirstComponent>
-      <SecondComponent></SecondComponent>
-      <ThirdComponent></ThirdComponent>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<h1>Tommy's Todo App</h1>
+			<FirstComponent></FirstComponent>
+			<ThirdComponent></ThirdComponent>
+		</div>
+	);
 }
 
 export default App;
